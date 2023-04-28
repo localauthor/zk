8e8572829c885fd630a393a28917cea30b24f249
Grant Rosson <grantrosson@gmail.com>
2023-04-28 23:18:42 +0300

    Garbage collect in zk--directory-files
    Prevents eventual slowdown

--- zk.el
+++ zk.el
@@ -375,6 +375,7 @@ When `zk-directory-recursive' is non-nil, searches recursively in
 subdirectories of `zk-directory' (except those matching
 `zk-directory-recursive-ignore-dir-regexp') and returns full
 file-paths."
+  (garbage-collect) ;; prevents eventual slowdown
   (let* ((regexp (or regexp zk-id-regexp))
          (list
           (if (not zk-directory-recursive)
